436ac9d83095c5f4c3c998789b27456ea993cd51
Jose Gutierrez <triforcegold@hotmail.com>
2016-07-09 16:34:49 -0700

    Added game clock and tick management.

---
+++ TetrisCS480Swift/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  TetrisCS480Swift
+//
+//  Created by Jose Gutierrez on 7/9/16.
+//  Copyright © 2016 CS480 Project. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
